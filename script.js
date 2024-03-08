0814cc74fb3975e5612fcd82a2fa7d68ddced05f
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2024-03-08 12:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let con = document.getElementById("hw");
+updateCon();
+
+window.addEventListener("resize", function() {
+    updateCon();
+});
+
+function updateCon() {
+    con.innerHTML = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+}
